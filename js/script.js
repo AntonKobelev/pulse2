7d8d32675e9f7c152585b6cca417feb5dfbead30
Anton <antonkobelev1985@gmail.com>
2022-02-10 10:57:49 +0500

    add modal and validate

--- js/script.js
+++ js/script.js
@@ -35,4 +35,51 @@ $(document).ready(function(){
 
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  // Modal
+  $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('fast');
+  });
+  $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('fast')
+  });
+
+  $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('fast');
+      })
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+              required: true,
+              minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+          name: {
+              required: "Пожалуйста, введите своё имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свой почтовый адрес",
+            email: "Неправильно введен адрес почты, формат name@domain.com"
+          }
+        }
+    });
+  }
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
 });
